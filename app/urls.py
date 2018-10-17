356a7b8868b4685bbdbf7d477de0cb6712af7045
cherucole <cheruyotcheruyot@gmail.com>
2018-10-17 16:46:53 +0300

    templates linked well

---
+++ app/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+
+
+    url(r'^$', views.homepage, name='homepage')
+    # url(r'^upload$', views.upload_image, name='upload'),
+    # url(r'^accounts/profile/(?P<username>\w+)', views.profile, name='my_profile'),
+    # url(r'^search_results/', views.search_results, name='search'),
+    # url(r'^like/(?P<operation>.+)/(?P<pk>\d+)',views.like, name='like'),
+    # url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    # url(r'^comment/(?P<pk>\d+)',views.add_comment,name='comment'),
+    # url(r'^new/profile$', views.add_profile, name='add_profile'),
+    # url(r'^follow/(?P<operation>.+)/(?P<id>\d+)', views.follow, name='follow'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+import datetime as dt
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+from .email import *
 
-# Create your views here.
+def homepage(request):
+    posts = "testing to link templates"
+
+    return render(request, 'index.html', {"posts":posts})
