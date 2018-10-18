5f689e88ab0a6b2534f051b2077d24d8bf350479
cherucole <cheruyotcheruyot@gmail.com>
2018-10-18 10:12:39 +0300

    Adding custom css

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import *
+
+# class CommentForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Comment
+#         exclude = ['poster', 'imagecommented']
+
+# class NewProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude = ['user']
+
+# class UploadForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         exclude = ['user_profile', 'profile','likes', 'opinions']
+
